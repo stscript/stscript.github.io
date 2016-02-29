f3711b5d9afa8c715519759eff41c9e3abf90e75
Tony <stscript@qq.com>
2016-02-29 15:56:55 +0800

    st

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var nunjucks = require('gulp-nunjucks');
+
 gulp.task('html',function(){
 	return gulp.src('templates/*.html')
 		.pipe(nunjucks.compile())
 		.pipe(gulp.dest('.'));
 });
-
+gulp.task('chenxing',function(){
+	return gulp.src('templates/chenxing/*.html')
+		.pipe(nunjucks.compile())
+		.pipe(gulp.dest('chengxing'));
+});
 gulp.task('watch',function(){
-	gulp.watch(['templates/*.html','templates/component/layout.nunjucks'],['html']);
+	gulp.watch(['templates/*.html','templates/component/*','templates/chenxing/*'],['html','chenxing']);
 });
 
 gulp.task('default',['watch']);
