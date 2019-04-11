9b29a2a4da20994380e0585f99d7848b0dbe29f7
suntong <stscript@qq.com>
2019-04-11 18:20:55 +0800

    up

---
+++ precache-manifest.f7852c4139234734e9ef90575974b8d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c539f7933aa51ecc25ba",
+    "url": "/static/js/main.0c48dca0.chunk.js"
+  },
+  {
+    "revision": "b6439638df1560e10f0b",
+    "url": "/static/js/2.ae6a4c33.chunk.js"
+  },
+  {
+    "revision": "c539f7933aa51ecc25ba",
+    "url": "/static/css/main.e25edca0.chunk.css"
+  },
+  {
+    "revision": "b6439638df1560e10f0b",
+    "url": "/static/css/2.490ea349.chunk.css"
+  },
+  {
+    "revision": "9b8899d6f8ea1ba3e6947027d94a45bf",
+    "url": "/index.html"
+  }
+];
